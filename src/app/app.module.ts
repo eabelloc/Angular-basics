9c73f8d864c0a8c28176a7e4a0567f4bd9f852c3
eabelloc <eabelloc.dev@gmail.com>
2022-10-27 17:42:16 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './constador/contador.module';
+
+//crear un module llamado contador
+//declaraciones y exportaciones
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
